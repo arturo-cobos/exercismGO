73e42b37652a76dda2eb61315aad2b8428639a5e
Arturo Cobos <arturo@co-boss.com>
2016-03-29 06:56:41 -0500

    Basic Clock

--- clock/clock.go
+++ clock/clock.go
@@ -4,23 +4,71 @@
 // You can document general stuff about the package here if you like.
 package clock
 
-// The value of testVersion here must match `targetTestVersion` in the file
-// clock_test.go.
+import "strconv"
+
 const testVersion = 3
 
-// Clock API as stub definitions.  No, it doesn't compile yet.
-// More details and hints are in clock_test.go.
+//Clock soporta la hroa y los minutos
+type Clock struct {
+    h,  m int
+}
+// Complete the type definition.  Pick a suitable data type.
 
-type Clock // Complete the type definition.  Pick a suitable data type.
-
+//New - asignar a struct
 func New(hour, minute int) Clock {
+   //for negative minutes  , add 60 minutes and substract 1 
+   for minute < 0 {
+       minute += 60
+       hour --
+   } 
+   //for minutes  , substract 60 minutes and add 1    
+   for minute > 59 {
+    minute -= 60
+    hour++
+   }
+   
+   //for negative hours, just make positive   
+   for hour < 0 {
+       hour +=  24  
+    }
+   hour = hour % 24
+        
+   return Clock {hour,minute}
+    
 }
 
-func (Clock) String() string {
+func (c Clock) String() string {
+
+   resultado :=    strconv.Itoa(c.h)
+   if len(resultado) == 1 {
+   resultado = "0" + resultado
+   }
+   resultado2 :=    strconv.Itoa(c.m)
+   if len(resultado2) == 1 {
+   resultado2 = "0" + resultado2
+   }
+    return resultado + ":" + resultado2
 }
 
-func (Clock) Add(minutes int) Clock {
+//Clock.add added minutes 
+func (c Clock) Add(minutes int) Clock {
+  //just add the minutes
+   c.m += minutes
+    
+   for c.m < 0 {
+       c.m += 60
+       c.h --
+   } 
+      
+   for c.m > 59 {
+    c.m -= 60
+    c.h++
+   }
+   
+   for c.h < 0 {
+       c.h +=  24  
+    }
+    c.h = c.h % 24
+    return c
 }
 
-// Remember to delete all of the stub comments.
-// They are just noise, and reviewers will complain.
